19462a8ec0dd0416fb55e5fa0c7b2626b735b23f
xkxsxux <simonova278@gmail.com>
2021-09-12 23:42:00 +0500

    fragment

---
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+            supportFragmentManager.beginTransaction().add(R.id.fragment_container_view,FragmentFirst()).commit()
+
+    }
+}
